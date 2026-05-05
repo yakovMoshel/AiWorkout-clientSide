5481e36584585fdcd4847b48532565488c1917b1
YakovMoshel <yakovmoshel123@gmail.com>
2026-05-05 13:02:43 +0300

    feat: loading if user is not auth

--- src/components/templates/main-layout.tsx
+++ src/components/templates/main-layout.tsx
@@ -1,11 +1,21 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "../molecules/Header";
+import { useAuth } from "../../store/auth-context";
 
 const AUTH_ROUTES = ["/login", "/register", "/setup"];
 
 export default function Layout() {
   const { pathname } = useLocation();
+  const { loading } = useAuth();
   const isAuthPage = AUTH_ROUTES.includes(pathname);
+
+  if (loading) {
+    return (
+      <main>
+        <Outlet />
+      </main>
+    );
+  }
 
   return (
     <>
